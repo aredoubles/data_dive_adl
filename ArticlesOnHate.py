a5fa4bc209727607eea07464cbfb5bd6f36a8a12
aredoubles <roger.w.shaw@gmail.com>
2017-04-29 15:57:59 -0400

    News sources

--- ArticlesOnHate.py
+++ ArticlesOnHate.py
@@ -1,6 +1,7 @@
 import requests
 import bs4
 import feedparser
+import pandas as pd
 
 
 # NY Times Hate Crimes feed
@@ -8,20 +9,26 @@ def nyt_hate():
     nytrss = feedparser.parse(
         'https://www.nytimes.com/svc/collections/v1/publish/http://www.nytimes.com/topic/subject/hate-crimes/rss.xml')
 
+    nythate = {}
     for i in range(20):
+        idlink = nytrss['entries'][i]['link']
         res = requests.get(nytrss['entries'][i]['link'])
         res.raise_for_status()
         starch = bs4.BeautifulSoup(res.text, 'html5lib')
         # The below is specific to the NYT
         thetext = starch.find_all(class_='story-body-text')
 
-        with open("NYT-Hate.txt", "a") as text_file:
-            for tag in thetext:
-                text_file.write(tag.text.strip())
-                text_file.write(' ')
+        full = ''
+        for tag in thetext:
+            full += (tag.text.strip() + ' ')
 
-            # Insert a line break after each article
-            text_file.write('\n\n')
+        nythate[idlink] = full
+
+    build = pd.DataFrame.from_dict(nythate, orient='index')
+    build.columns = ['Text']
+    build['Source'] = 'New York Times'
+    build['Hate crime'] = 1
+    build.to_csv('NYT1.csv')
 
 
 # NY Times feed on NY local news
@@ -29,20 +36,26 @@ def nyt_local():
     nytrss = feedparser.parse(
         'https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/nyregion/rss.xml')
 
+    nytnon = {}
     for i in range(20):
+        idlink = nytrss['entries'][i]['link']
         res = requests.get(nytrss['entries'][i]['link'])
         res.raise_for_status()
         starch = bs4.BeautifulSoup(res.text, 'html5lib')
         # The below is specific to the NYT
         thetext = starch.find_all(class_='story-body-text')
 
-        with open("NYT-NewYork.txt", "a") as text_file:
-            for tag in thetext:
-                text_file.write(tag.text.strip())
-                text_file.write(' ')
+        full = ''
+        for tag in thetext:
+            full += (tag.text.strip() + ' ')
 
-            # Insert a line break after each article
-            text_file.write('\n\n')
+        nytnon[idlink] = full
+
+    build = pd.DataFrame.from_dict(nytnon, orient='index')
+    build.columns = ['Text']
+    build['Source'] = 'New York Times'
+    build['Hate crime'] = 0
+    build.to_csv('NYT0.csv')
 
 
 # Guardian feed on hate crime
---
+++ NewsSources.md
@@ -0,0 +1,23 @@
+### News sources with tags
+
+https://www.nytimes.com/topic/subject/hate-crimes
+
+https://www.theguardian.com/society/hate-crime
+236 results
+
+http://abcnews.go.com/topics/news/issues/hate-crimes.htm
+
+
+
+
+### No RSS
+
+http://articles.latimes.com/keyword/hate-crimes
+
+http://www.huffingtonpost.com/topic/hate-crimes
+
+http://articles.baltimoresun.com/keyword/hate-crimes
+
+http://www.nydailynews.com/tags/hate-crimes
+
+https://www.usnews.com/topics/subjects/hate_crimes
